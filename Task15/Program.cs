c9f0af15a6550cf2ae44837bd821563271338376
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-11-28 00:39:27 +0300

    add Task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Enter day of the week: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.Write("Wrong!\nEnter day of the week: ");
+    n = Convert.ToInt32(Console.ReadLine());    
+}
+if (n <= 5)
+{
+    Console.WriteLine("No Holiday. I need go to work.");
+}
+else 
+{
+    Console.WriteLine("Holiday!!!!");
+}
