c9dc41d69c7fb95dd50f614240c1d799bbf9727a
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-11-28 01:18:31 +0300

    add Additionally

---
+++ Additionally/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int secondMax = 0;
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > max)
+    {
+        secondMax = max;
+        max = n; 
+    }
+    else
+    {
+        if (n > secondMax)
+        {
+            secondMax = n; 
+        }
+    }
+}
+Console.WriteLine("End");
+Console.WriteLine($"Second maximum = {secondMax}");
