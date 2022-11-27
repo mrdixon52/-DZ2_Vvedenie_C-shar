3c414b2fa6382540884abfbea0a8f46bfb2631f7
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-11-27 20:59:26 +0300

    Add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Enter three-digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1000 && n > 100)
+{
+    Console.Write("The second digit of this number: ");
+    Console.Write(n / 10 % 10);
+}
+else 
+{
+    Console.WriteLine("Three-digit number!");
+}
